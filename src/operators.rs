c748739e0551345a384a476bdabe8bac7a5c830d
emily Crandall Fleischman <zyxw59@gmail.com>
2024-07-22 11:02:28 -0400

    remove `Dot` precedence level

--- src/operators.rs
+++ src/operators.rs
@@ -17,8 +17,6 @@ pub enum Precedence {
     Multiplicative,
     /// Exponential operators, such as `^`, as well as unary sine and cosine.
     Exponential,
-    /// The `.` operator
-    Dot,
 }
 
 macro_rules! as_binary_operator {
