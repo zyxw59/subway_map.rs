bc88d355e39ae43525107d78431a2120664ec387
emily Crandall Fleischman <zyxw59@gmail.com>
2025-07-14 11:18:09 -0400

    remove `as_binary_operator` and `as_unary_operator` macros

--- src/operators.rs
+++ src/operators.rs
@@ -22,31 +22,13 @@ pub enum Precedence {
     Field,
 }
 
-macro_rules! as_binary_operator {
-    ($fn:path) => {{
-        fn _f(a: Value, b: Value, _: &mut dyn EvaluationContext) -> Result {
-            $fn(a, b)
-        }
-        _f
-    }};
-}
-
-macro_rules! as_unary_operator {
-    ($fn:path) => {{
-        fn _f(a: Value, _: &mut dyn EvaluationContext) -> Result {
-            $fn(a)
-        }
-        _f
-    }};
-}
-
 pub const COMMA: BinaryOperator = BinaryOperator {
-    function: as_binary_operator!(Value::comma),
+    function: |a, b, _| a.comma(b),
     name: Variable::new_static(","),
 };
 
 pub const COMMA_UNARY: UnaryOperator = UnaryOperator::Function(NamedFunction {
-    function: as_unary_operator!(Value::comma_unary),
+    function: |a, _| a.comma_unary(),
     name: Variable::new_static(","),
 });
 
@@ -108,22 +90,22 @@ impl BinaryOperator {
         use self::Precedence::*;
         use expr_parser::operator::Fixity::*;
         get_binary_builtin!(match key {
-            "==" => (Left(Comparison), as_binary_operator!(Value::eq)),
-            "!=" => (Left(Comparison), as_binary_operator!(Value::ne)),
-            "<" => (Left(Comparison), as_binary_operator!(Value::lt)),
-            "<=" => (Left(Comparison), as_binary_operator!(Value::le)),
-            ">" => (Left(Comparison), as_binary_operator!(Value::gt)),
-            ">=" => (Left(Comparison), as_binary_operator!(Value::ge)),
+            "==" => (Left(Comparison), |a, b, _| a.eq(b)),
+            "!=" => (Left(Comparison), |a, b, _| a.ne(b)),
+            "<" => (Left(Comparison), |a, b, _| a.lt(b)),
+            "<=" => (Left(Comparison), |a, b, _| a.le(b)),
+            ">" => (Left(Comparison), |a, b, _| a.gt(b)),
+            ">=" => (Left(Comparison), |a, b, _| a.ge(b)),
             "max" => (Left(Comparison), Value::max),
             "min" => (Left(Comparison), Value::min),
             "+" => (Left(Additive), Value::add),
             "-" => (Left(Additive), Value::sub),
-            "++" => (Left(Additive), as_binary_operator!(Value::hypot)),
-            "+-+" => (Left(Additive), as_binary_operator!(Value::hypot_sub)),
+            "++" => (Left(Additive), |a, b, _| a.hypot(b)),
+            "+-+" => (Left(Additive), |a, b, _| a.hypot_sub(b)),
             "*" => (Left(Multiplicative), Value::mul),
             "/" => (Left(Multiplicative), Value::div),
             "&" => (Left(Multiplicative), Value::intersect),
-            "^" => (Right(Exponential), as_binary_operator!(Value::pow)),
+            "^" => (Right(Exponential), |a, b, _| a.pow(b)),
             "<>" => (Left(Exponential), Value::line_between),
             ">>" => (Left(Exponential), Value::line_vector),
             "^^" => (Left(Exponential), Value::line_offset),
@@ -148,12 +130,12 @@ impl UnaryOperator {
     pub fn get(key: &str) -> Option<(Precedence, Self)> {
         get_unary_builtin!(match key {
             "-" => (Multiplicative, Value::neg),
-            "xpart" => (Multiplicative, as_unary_operator!(Value::xpart)),
-            "ypart" => (Multiplicative, as_unary_operator!(Value::ypart)),
-            "cos" => (Exponential, as_unary_operator!(Value::cos)),
-            "sin" => (Exponential, as_unary_operator!(Value::sin)),
+            "xpart" => (Multiplicative, |a, _| a.xpart()),
+            "ypart" => (Multiplicative, |a, _| a.ypart()),
+            "cos" => (Exponential, |a, _| a.cos()),
+            "sin" => (Exponential, |a, _| a.sin()),
             "dir" => (Exponential, Value::dir),
-            "angle" => (Exponential, as_unary_operator!(Value::angle)),
+            "angle" => (Exponential, |a, _| a.angle()),
         })
     }
 }
